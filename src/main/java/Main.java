c35191bdd116aa9c6a30f43fc93267c7e24c2571
Fernando Ortega <fortegarequena@gmail.com>
2022-12-05 23:38:15 +0100

    Corrección de errores y mejora de estilo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,4 @@
 import ormExpressCorreos.Controller;
-import ormExpressCorreos.model.Usuario_generico;
-import ormExpressCorreos.model.Usuario_identificado;
-import ormExpressCorreos.model.Direccion;
 
 import java.sql.SQLException;
 
@@ -10,11 +7,15 @@ public class Main {
     public static void main (String [] args) {
         try {
             Controller controlador= new Controller();
-            //@TODO añada las llamadas a las funciones de la clase Controller.java necesarias
-            //para la creación del usuario identificado y la direccion.
-            if(Usuario_identificado!=null && direccion!=null)
-                System.out.println("Se ha creado el usuario identificado " + usuario.getnombre() + " con DNI " + usuario.getdni() + 
-                " que vive en " + direccion.getdireccioncompleta());
+
+            // @TODO añada las llamadas a las funciones de la clase Controller.java necesarias
+            // para la creación del usuario identificado y la direccion.
+
+            if(usuarioIdentificado != null && direccion != null) {
+                System.out.println("Se ha creado el usuario identificado " + usuario.getNombre() + " con DNI "
+                        + usuario.getDNI() + " que vive en " + direccion.getDireccionCompleta());
+            }
+
         } catch(SQLException e) {
             System.err.println("Se ha producido un error en la conexión con la base de datos");
             e.printStackTrace();
